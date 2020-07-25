26bf5493203211ef43e8ce999baa1a96a910ffbf
1000ch <shogosensui@gmail.com>
2020-07-26 02:59:52 +0900

    Various tweaks

--- test/test.js
+++ test/test.js
@@ -1,9 +1,8 @@
 'use strict';
-
-var fs = require('fs');
+const fs = require('fs');
 
 exports.cwebp = {
-  convertWebP: function (test) {
+  convertWebP: test => {
     test.expect(1);
     test.ok(fs.existsSync('tmp/test.png'), 'should convert WebP images');
     test.done();
